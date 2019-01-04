f306ab1ba5b326a918033663fafc41a148f0f3d4
ambercui <amber.cui@softwareofexcellence.com>
2019-01-04 17:41:29 +1300

    change the module

---
+++ my-app/src/app/language-page/language-page-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//import { HomePageDetailComponent } from './home-page-detail/home-page-detail.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageDetailComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LanguagePageRoutingModule { }
